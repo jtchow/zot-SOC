b1fabd1de0ac3ed5def53d60627555499f52eca5
jtchow <jason.chow@securecircle.com>
2019-10-26 20:27:42 -0700

    linked form to flask/python

--- main.py
+++ main.py
@@ -15,21 +15,20 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)   #initialize flask app 
 bootstrap = Bootstrap(app)              #dont think i need this lol
 app.config['SECRET_KEY'] = 'hard to guess string'               #or this
+   
 
-class NameForm(FlaskForm):
-    degreeworks = TextAreaField(label='',validators=[DataRequired()], render_kw={"rows": 30, "cols": 90})
-    submit = SubmitField('Submit')
+
+
+@app.route('/')             #if the url is zotsoc.appspot.com/ then show the form and have them enter input for my function and then display necessary classes
+def index():
+    return render_template('homepage.html')
+
     
 
-
-
-@app.route('/', methods=['GET','POST'])             #if the url is zotsoc.appspot.com/ then show the form and have them enter input for my function and then display necessary classes
-def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        degreeworks = form.degreeworks.data
-        courses = read_input(degreeworks)
-        return render_template('index2.html',courses=courses)
-    return render_template('homepage.html',form=form)
+@app.route('/', methods=['POST'])
+def show_output():
+    degreeworks = request.form['degreeworks']
+    courses = read_input(degreeworks)
+    return render_template('index2.html',courses=courses)
 
 
